941c6e34df14e9cd6fc59d762224f82daf61a686
Codegobli <afolabisamson20@gmail.com>
2022-06-24 23:33:53 -0700

    Create script.js
    initial  commit

---
+++ script.js
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input your name: ");
+    String name = input.next();
+    System.out.print("Input your height: ");
+
+System.out.print("Input your height: ");
+    String lname = input.next();
+
+    String lname = input.next();
+    System.out.println();
+    System.out.println("+name+"+height+" "+country);
+  }
+}
